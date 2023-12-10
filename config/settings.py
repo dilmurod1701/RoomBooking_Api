2f5d54729fbeff6634ed0481bd6d040d7bc2dced
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-10 22:25:35 +0500

    Database Changed

--- config/settings.py
+++ config/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'config.wsgi.app'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'room',
-        'USER': 'postgres',
-        'PASSWORD': 'hacker',
-        'HOST': 'localhost',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': '63ytPqgxTFkA',
+        'HOST': 'ep-flat-snowflake-37663922-pooler.us-east-1.postgres.vercel-storage.com',
     }
 }
 
